c7c0fd02864833a79249650208c0225d0ca2d0e6
sonyujeong <sonyujeong18@amsquare.ai>
2025-04-08 16:46:50 +0900

    feat: json server에서 데이터 가져오기

--- part1_wedding/src/App.tsx
+++ part1_wedding/src/App.tsx
@@ -1,31 +1,43 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 import classNames from 'classnames/bind';
 
 import styles from './App.module.scss';
+import FullScreenMessage from './components/shared/FullScreenMessage';
 
 const cx = classNames.bind(styles);
 
 function App() {
-  return (
-    <div className={cx('container')}>
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const [wedding, setWedding] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(false);
+  // 1. 데이터 호출
+  useEffect(() => {
+    setLoading(true);
+
+    fetch('http://localhost:8888/wedding')
+      .then((res) => {
+        if (res.ok === false) {
+          throw new Error('청첩장 정보를 불러오지 못했습니다.');
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setWedding(data);
+      })
+      .catch((e) => {
+        console.log(e);
+        setError(true);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading === false) return <FullScreenMessage type="loading" />;
+
+  if (error) return <FullScreenMessage type="error" />;
+
+  return <div className={cx('container')}>{JSON.stringify(wedding)}</div>;
 }
 
 export default App;
