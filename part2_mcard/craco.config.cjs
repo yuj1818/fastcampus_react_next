d75e32154bbd9a0311d87a664151d2fe8f2305cd
sonyujeong <sonyujeong18@amsquare.ai>
2025-04-16 16:20:00 +0900

    init: 프로젝트 초기 환경 설정

---
+++ part2_mcard/craco.config.cjs
@@ -0,0 +1,22 @@
+const cracoAlias = require('craco-alias');
+
+module.exports = {
+  plugins: [
+    {
+      plugin: cracoAlias,
+      options: {
+        source: 'tsconfig',
+        tsConfigPath: 'tsconfig.paths.json',
+      },
+    },
+  ],
+  babel: {
+    presets: [
+      [
+        '@babel/preset-react',
+        { runtime: 'automatic', importSource: '@emotion/react' },
+      ],
+    ],
+    plugins: ['@emotion/babel-plugin'],
+  },
+};
---
+++ part2_mcard/src/App.tsx
@@ -0,0 +1,44 @@
+import React from 'react';
+import { css } from '@emotion/react';
+import styled from '@emotion/styled';
+import logo from './logo.svg';
+import './App.css';
+
+const bold = css`
+  font-weight: bold;
+`;
+
+const containerStyles = css`
+  background-color: pink;
+  ${bold};
+`;
+
+const Button = styled.button`
+  width: 200px;
+  height: 100px;
+  ${bold};
+`;
+
+function App() {
+  return (
+    <div className="App" css={containerStyles}>
+      <Button>스타일 버튼</Button>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
