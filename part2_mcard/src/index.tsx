95967769df97efbfc4c76eb70ecdf3e48066285c
sonyujeong <sonyujeong18@amsquare.ai>
2025-04-16 17:39:13 +0900

    style: 컬러 팔레트 설정

--- part2_mcard/src/index.tsx
+++ part2_mcard/src/index.tsx
@@ -3,12 +3,15 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Global } from '@emotion/react';
+import globalStyles from './styles/globalStyles';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 root.render(
   <React.StrictMode>
+    <Global styles={globalStyles} />
     <App />
   </React.StrictMode>,
 );
